bec7606f255c96a5c88e03d23c7c37c32d1742b6
thanikc <thanik.cheowtirakul@attempto.eu>
2018-09-28 12:45:05 +0200

    Initial Commit

---
+++ AnimatedMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimatedMenu
+//
+//  Created by Thanik Cheowtirakul on 28.09.18.
+//  Copyright © 2018 attempto GmbH & Co. KG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
